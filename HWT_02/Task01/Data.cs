80f5c796f847639d6b388bdb6bba6fd39bb50e50
Unstoppable641 <car641@mail.ru>
2017-12-03 21:48:13 +0400

    HWT_02

---
+++ HWT_02/Task01/Data.cs
@@ -0,0 +1,30 @@
+﻿namespace Task01
+{
+    using System;
+
+    public class Data
+    {
+        public static void ThrowExA(int a)
+        {
+            if (a <= 0)
+            {
+                throw new System.ArgumentException("[a] should not be less than or equal to zero.\n");
+            }
+        }
+
+        public static void ThrowExB(int b)
+        {
+            if (b <= 0)
+            {
+                throw new System.ArgumentException("[a] should not be less than or equal to zero.\n");
+            }
+        }
+
+        public static void GetArea(int a, int b)
+        {
+            int area = a * b;
+            Console.Write($"\nThe area of the rectangle is {area}\n");
+            Console.WriteLine("\n");
+        }
+    }
+}
---
+++ HWT_02/Task06/Data.cs
@@ -0,0 +1,89 @@
+﻿namespace Task06
+{
+    using System;
+
+    public class Data
+    {
+        public static string Style = "None";
+        public static bool Bold;
+        public static bool Italic;
+        public static bool Underline;
+        public static int N;
+        public static bool UsingStyles = true;
+
+        public static void Print()
+        {
+            Console.WriteLine($"Parameters:  {Style}\nchoose:\n\t 1:  bold\n\t 2:  italic\n\t 3:  underline\n\t 0:  Exit");
+        }
+
+        public static void SelectedStyle()
+        {
+            int.TryParse(Console.ReadLine(), out N);
+            switch (N)
+            {
+                case 1:
+                    StyleCheck("Bold", ref Bold);
+                    break;
+                case 2:
+                    StyleCheck("Italic", ref Italic);
+                    break;
+                case 3:
+                    StyleCheck("Underline", ref Underline);
+                    break;
+                case 0:
+                    UsingStyles = false;
+                    break;
+            }
+        }
+
+        public static void StyleCheck(string str, ref bool check)
+        {
+            string str2 = ", " + str + ",";
+            string str3 = ", " + str;
+            string str4 = str + ", ";
+            string str5 = string.Empty;
+            var none = Style == "None";
+            var empty = Style == str5;
+
+            if (check)
+            {
+                if (Style.Contains(str2))
+                {
+                    Style = Style.Replace(str2, str5);
+                }
+                else if (Style.Contains(str3))
+                {
+                    Style = Style.Replace(str3, str5);
+                }
+                else if (Style.Contains(str4))
+                {
+                    Style = Style.Replace(str4, str5);
+                }
+                else if (Style.Contains(str))
+                {
+                    Style = Style.Replace(str, str5);
+                }
+
+                check = false;
+
+                if ((!Italic) && (!Underline) && (!Bold))
+                {
+                    Style = "None";
+                }
+            }
+            else
+            {
+                check = true;
+
+                if (none || empty)
+                {
+                    Style = str;
+                }
+                else
+                {
+                    Style = Style + str3;
+                }
+            }
+        }
+    }
+}
