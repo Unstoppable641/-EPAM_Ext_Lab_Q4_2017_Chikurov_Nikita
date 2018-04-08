fe8873236759e9505d67d2fc283f9929de31f3ff
Unstoppable641 <car641@mail.ru>
2018-04-08 21:01:29 +0400

    DOES NOT WORK(1)

---
+++ HWT_13/Task01/Helpers/PageChanger.cs
@@ -0,0 +1,34 @@
+﻿namespace Task01.Helpers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using Task01.Models;
+    using System.Text;
+    using System.Web.Mvc;
+
+    public static class PagingHelpers
+    {
+        public static MvcHtmlString PageLinks(this HtmlHelper html,
+            PageInfo pageInfo, Func<int, string> pageUrl)
+        {
+            var result = new StringBuilder();
+            for (var i = 1; i <= pageInfo.TotalPages; i++)
+            {
+                var tag = new TagBuilder("a");
+                tag.MergeAttribute("href", pageUrl(i));
+                tag.InnerHtml = i.ToString();
+                if (i == pageInfo.PageNumber)
+                {
+                    tag.AddCssClass("selected");
+                    tag.AddCssClass("btn-primary");
+                }
+
+                tag.AddCssClass("btn btn-default");
+                result.Append(tag.ToString());
+            }
+
+            return MvcHtmlString.Create(result.ToString());
+        }
+    }
+}
